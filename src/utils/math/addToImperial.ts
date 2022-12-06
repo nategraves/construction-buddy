01943e8448db3f02d8a43c4d3dcd11765656f2bf
Nate Graves <natesgraves@gmail.com>
2022-12-05 20:48:28 -0800

    Some more stuff before switching approach

---
+++ src/utils/math/addToImperial.ts
@@ -0,0 +1,35 @@
+import { MetricValue, ImperialValue } from "../../types";
+import { isMetric } from "../types";
+import { convert } from "./convert";
+
+export const addToImperial = ({
+  value,
+  toAdd: toAddInitial,
+}: {
+  value: ImperialValue;
+  toAdd: MetricValue | ImperialValue;
+}): ImperialValue => {
+  const toAdd = isMetric(toAddInitial)
+    ? (convert(toAddInitial) as ImperialValue)
+    : toAddInitial;
+
+  if (toAdd.ft || value.ft) {
+    value.ft = toAdd.ft ?? 0 + value.ft ?? 0;
+  }
+
+  if (toAdd.in || value.in) {
+    value.in = toAdd.in ?? 0 + value.in ?? 0;
+
+    if (value.in >= 12) {
+      value.ft += Math.round(value.in / 12);
+      value.in = value.in % 12;
+    }
+
+    if (typeof toAdd.n === "number" && typeof toAdd.d === "number") {
+      value.n = value.n ?? 0 + toAdd.n ?? 0;
+      value.d = value.d ?? 0 + toAdd.d ?? 0;
+    }
+  }
+
+  return value;
+};
