4b670d423760d82643f0260a4dd325fd4b5ca321
Nate Graves <natesgraves@gmail.com>
2023-01-01 23:07:12 -0800

    Finish adding tests for multiply

--- src/data/Value/tests/multiply.test.ts
+++ src/data/Value/tests/multiply.test.ts
@@ -16,40 +16,32 @@ describe("multiply correctly multiplies", () => {
   test("ImperialValue,ImperialValue", () => {
     const value: ImperialValue = { ft: 8, ins: 8, fr: fraction(1, 2) };
     const toApply: ImperialValue = { ft: 2, ins: 2, fr: fraction(1, 3) };
-    const expected = "19.109954";
+    const expected = "2751.833333";
     const result = multiply({ value, toApply }) as number;
     expect(result.toFixed(6)).toBe(expected);
   });
 
-  //   test("ImperialValue,Number", () => {
-  //     const value: ImperialValue = { ft: 8, ins: 8, fr: fraction(1, 2) };
-  //     const toApply = 2;
-  //     const expected: ImperialValue = { ft: 4, ins: 4, fr: fraction(1, 4) };
-  //     const result = multiply({ value, toApply }) as ImperialValue;
-  //     expect(JSON.stringify(result)).toBe(JSON.stringify(expected));
-  //   });
+  test("ImperialValue,Number", () => {
+    const value: ImperialValue = { ft: 8, ins: 8, fr: fraction(1, 2) };
+    const toApply = 2;
+    const expected: ImperialValue = { ft: 17, ins: 5, fr: fraction(0, 1) };
+    const result = multiply({ value, toApply }) as ImperialValue;
+    expect(JSON.stringify(result)).toBe(JSON.stringify(expected));
+  });
 
-  //   test("Handles dividing evenly", () => {
-  //     const value: ImperialValue = { ft: 5 };
-  //     const toApply = 5;
-  //     const expected: ImperialValue = { ft: 1, ins: 0, fr: fraction(0, 1) };
-  //     const result = multiply({ value, toApply }) as ImperialValue;
-  //     expect(JSON.stringify(result)).toBe(JSON.stringify(expected));
-  //   });
+  test("MetricValue,MetricValue", () => {
+    const value: MetricValue = { m: 8, cm: 8, mm: 8 };
+    const toApply: MetricValue = { m: 2, cm: 2, mm: 2 };
+    const expected = 163539.36;
+    const result = multiply({ value, toApply });
+    expect(JSON.stringify(result)).toBe(JSON.stringify(expected));
+  });
 
-  //   test("MetricValue,MetricValue", () => {
-  //     const value: MetricValue = { m: 8, cm: 8, mm: 8 };
-  //     const toApply: MetricValue = { m: 2, cm: 2, mm: 2 };
-  //     const expected = 4;
-  //     const result = multiply({ value, toApply });
-  //     expect(JSON.stringify(result)).toBe(JSON.stringify(expected));
-  //   });
-
-  //   test("MetricValue,Number", () => {
-  //     const value: MetricValue = { m: 8, cm: 8, mm: 2 };
-  //     const toApply: number = 2;
-  //     const expected: MetricValue = { m: 4, cm: 4, mm: 1 };
-  //     const result = multiply({ value, toApply }) as MetricValue;
-  //     expect(JSON.stringify(result)).toBe(JSON.stringify(expected));
-  //   });
+  test("MetricValue,Number", () => {
+    const value: MetricValue = { m: 8, cm: 8, mm: 2 };
+    const toApply: number = 2;
+    const expected: MetricValue = { m: 16, cm: 16, mm: 4 };
+    const result = multiply({ value, toApply }) as MetricValue;
+    expect(JSON.stringify(result)).toBe(JSON.stringify(expected));
+  });
 });
